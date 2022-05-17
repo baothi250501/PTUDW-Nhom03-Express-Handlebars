55c12a030c5a4e80361a4b5f1dd5b12eba0e1075
Nguyễn Lê Bảo Thi <thinguyenlebao@gmail.com>
2022-05-17 17:45:28 +0700

    add layouts + hbs

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const express = require('express')
-const app = express();
 const path = require('path')
+const expressHbs = require('express-handlebars');
 
-app.use(express.static(path.join(__dirname, 'Handlebars-StaticFiles'),{
-    index: 'index.htm'
-}))
+const app = express();
 
-/*app.get('/', (req, res) => {
-    res.send('hello')
-})*/
+const hbs = expressHbs.create({
+    layoutsDir: path.join(__dirname, 'views/layouts'),
+    extname: 'hbs',
+    defaultLayout: 'layout'
+})
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, 'Handlebars-StaticFiles')))
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/task1', (req, res) => {
+    res.render('task1')
+})
+
+app.get('/task2', (req, res) => {
+    res.render('task2')
+})
+
+app.get('/task3', (req, res) => {
+    res.render('task3')
+})
+
+app.get('/task4', (req, res) => {
+    res.render('task4')
+})
+
+app.get('/task4-details', (req, res) => {
+    res.render('task4-details')
+})
 
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), () => {
